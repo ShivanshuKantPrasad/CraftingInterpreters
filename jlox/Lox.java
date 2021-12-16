f8d4cb8b04055a4e8f7ec172dea0473b9c884325
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2021-12-16 21:22:30 +0530

    Scanning

--- jlox/Lox.java
+++ jlox/Lox.java
@@ -9,4 +9,59 @@ import java.nio.file.Paths;
 import java.util.List;
 
 public class Lox{
+
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        if( args.length > 1 ){
+            System.out.println("Usage: jlox [script]");
+            System.exit(64);
+        } else if( args.length == 1 ){
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runPrompt() throws IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for(;;){
+            System.out.print("> ");
+            String line = reader.readLine();
+            if(line == null) break;
+            run(line);
+            hadError = false;
+        }
+
+
+    }
+
+    private static void runFile(String arg) throws IOException {
+        byte[] bytes = Files.readAllBytes(Paths.get(arg));
+        run(new String(bytes, Charset.defaultCharset()));
+
+        // Indicate an error in the exit code.
+        if (hadError) System.exit(65);
+    }
+
+    private static void run(String s) {
+        Scanner scanner = new Scanner(s);
+        List<Token> tokens = scanner.scanTokens();
+        for(Token token : tokens){
+            System.out.println(token);
+        }
+
+    }
+
+    static void error(int line, String message){
+        report(line, "", message);
+    }
+
+    private static void report(int line, String where, String message){
+        System.err.println("[line " + line + "] Error" + where + ": " + message);
+        hadError = true;
+    }
+
 }
