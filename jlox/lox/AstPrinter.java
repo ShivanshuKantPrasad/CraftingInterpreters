323e6c117def603c1bf9c6023ef13f35a31ae4de
Shivanshu Kant Prasad <shivanshukantprasad@gmail.com>
2024-05-28 19:39:39 +0530

    Implement Inheritance

--- jlox/lox/AstPrinter.java
+++ jlox/lox/AstPrinter.java
@@ -46,6 +46,11 @@ public class AstPrinter implements Expr.Visitor<String> {
     }
 
     @Override
+    public String visitSuperExpr(Expr.Super expr) {
+        return parenthesize("SUPER");
+    }
+
+    @Override
     public String visitThisExpr(Expr.This expr) {
         return parenthesize("This");
     }
